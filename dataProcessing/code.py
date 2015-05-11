ed4f7ffc6e079e90dd0f63cc488dca67e470741a
acatinfd <xiaorong@utexas.edu>
2015-05-10 20:58:26 -0500

    preprocess data

---
+++ dataProcessing/code.py
@@ -0,0 +1,47 @@
+import pickle
+
+#get business
+all_business = {}
+with open("yelp_academic_dataset_business.json") as f:
+    for raw in f:
+        raw = raw.replace('true', 'True').replace('false', 'False')
+        b = eval(raw)
+        if 'Restaurants' in b['categories']:
+            all_business[b['business_id']] = b
+            all_business[b['business_id']]['tips'] = []
+            all_business[b['business_id']]['reviews'] = []
+len(all_business)
+
+#get tips for business
+with open("yelp_academic_dataset_tip.json") as f:
+    count = 0
+    for data in f:
+        data = data.replace('\n','')
+        t = eval(data)
+        if t['business_id'] in all_business:
+            count = count + 1
+            tip = t['text']
+            all_business[t['business_id']]['tips'].append(tip)
+count   
+
+#get reviews for business
+with open("yelp_academic_dataset_review.json") as f:
+    reviewCount = 0
+    for data in f:
+        data = data.replace('\n','')
+        review = eval(data)
+        if review['business_id'] in all_business:
+            reviewCount = reviewCount + 1
+            all_business[t['business_id']]['reviews'].append(review)
+reviewCount
+
+#save data
+pickle.dump( all_business, open( "saveBusiness.p", "wb") )
+
+"""
+How to load data:
+----------------
+import pickle
+all_business = pickle.load( open( "saveBusiness.p", "rb" ) )
+----------------
+"""
